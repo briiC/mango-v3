434835c73079d58443f1e571d880b70d4de62861
Mareks Ruskuls <mareks@nic.lv>
2019-12-06 13:06:51 +0200

    Remove TAB and \R from content before parsing to md

--- Markdown.go
+++ Markdown.go
@@ -1,11 +1,15 @@
 package mango
 
 import (
+	"bytes"
+
 	"github.com/gomarkdown/markdown"
 	"github.com/gomarkdown/markdown/parser"
 )
 
 func parseMarkdown(buf []byte) []byte {
 	parser := parser.NewWithExtensions(parser.CommonExtensions | parser.AutoHeadingIDs)
+	buf = bytes.ReplaceAll(buf, []byte("\t"), []byte("    "))
+	buf = bytes.ReplaceAll(buf, []byte("\r"))
 	return markdown.ToHTML(buf, parser, nil)
 }
